96a6b0fd3af88d7b4bb1688be10faba124508d5b
Xabier Fernández <jjbier@gmail.com>
2016-01-18 21:58:25 +0100

    first commit

---
+++ src/main/java/com/confirmsign/configuration/PersistenceJPAConfig.java
@@ -0,0 +1,93 @@
+package com.confirmsign.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.Properties;
+
+/**
+ * Created by xabier on 18/01/16.
+ */
+@Configuration
+@EnableTransactionManagement
+public class PersistenceJPAConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "com.confirmsign.core.model" });
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername( "root" );
+        dataSource.setPassword( "hormiga" );
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
+// Configure pull
+//
+//        dataSourceClassName=com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+//        dataSource.url=jdbc:mysql://localhost/database
+//        dataSource.user=test
+//        dataSource.password=test
+//        dataSource.cachePrepStmts=true
+//        dataSource.prepStmtCacheSize=250
+//        dataSource.prepStmtCacheSqlLimit=2048
+        return properties;
+    }
+
+//    private Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+//        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+//        return properties;
+//    }
+}
---
+++ src/main/java/com/confirmsign/core/model/PattHilos.java
@@ -0,0 +1,12 @@
+package com.confirmsign.core.model;
+
+/**
+ * Created by xabier on 18/01/16.
+ */
+public class PattHilos {
+
+    private long id;
+
+    private String name;
+
+}
